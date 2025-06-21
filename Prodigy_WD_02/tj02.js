63d2982a474945ced7e0ceff63e8a6add5e812ef
VaibsGit <morevaibhavi2005@gmail.com>
2025-06-21 13:21:24 +0530

    Refactor stopwatch functionality: updated HTML,  CSS, and JS for improved UI and behavior

---
+++ Prodigy_WD_02/tj02.js
@@ -0,0 +1,35 @@
+let seconds = 0;
+let timer = null;
+let display = document.getElementById("display");
+
+function updateTime(){
+    seconds++;
+
+    let hrs = Math.floor(seconds/3600);
+    let mins = Math.floor((seconds % 3600) / 60);
+    let secs = seconds % 60;
+
+    let h = String(hrs).padStart(2,'0');
+    let m = String(mins).padStart(2,'0');
+    let s =String(secs).padStart(2,'0');
+
+    display.textContent=`${h}:${m}:${s}`;
+}
+
+function start() {
+    if (timer === null) return; {
+        timer = setInterval(updateTime, 1000);
+    }
+
+function pause(){
+    clearInterval(timer);
+    timer = null;
+}
+
+function reset() {
+    clearInterval(timer);
+    timer = null;
+    seconds = 0;
+    display.textContent = `00:00:00`;
+}
+}
